0800045061ae26c23f4785c27d3cbd82eda63f59
hobbestigrou <natal.ngetal@novapost.fr>
2014-10-29 16:42:25 +0100

    [Core] Refactoring be more generic use a json file (fixes #7).

--- peoplespider/middlewares.py
+++ peoplespider/middlewares.py
@@ -1,23 +1,14 @@
 # import ipdb
 import re
-import json
 
 from scrapy.exceptions import IgnoreRequest
 
 
 class PeoplePattern(object):
-
     visited_patterns = []
 
-    def __init__(self):
-        with open('peoplespider/configs/peopleask_manager.json') as fil:
-            data = json.load(fil)
-
-        self.patterns = data['patterns']
-        self.excluded_patterns = data['excluded_patterns']
-
     def process_request(self, request, spider):
-        for pattern in self.patterns:
+        for pattern in spider.config['patterns']:
             if bool(re.search(str(pattern), request.url)):
                 if pattern in self.visited_patterns:
                     raise IgnoreRequest()
@@ -25,6 +16,6 @@ class PeoplePattern(object):
                     self.visited_patterns.append(pattern)
                     break
 
-        for excluded_pattern in self.excluded_patterns:
+        for excluded_pattern in spider.config['excluded_patterns']:
             if bool(re.search(str(excluded_pattern), request.url)):
                 raise IgnoreRequest()
--- peoplespider/spiders/people.py
+++ peoplespider/spiders/people.py
@@ -1,4 +1,5 @@
-#import ipdb
+import json
+import urlparse
 
 from scrapy.spider import Spider
 from scrapy.http import FormRequest, Request
@@ -6,69 +7,60 @@ from scrapy.selector import Selector
 from scrapy import log
 
 
-class PeopleAskEmployee(Spider):
-    name = 'peopleask_employee'
-    allowed_domains = ['peopleask.local']
-    start_urls = ['http://peopleask.local/employee']
+class CrapySpider(Spider):
+    name = 'crapy_spider'
+
+    def __init__(self, rule=None):
+        super(CrapySpider, self).__init__()
+
+        if rule:
+            with open(rule) as fil:
+                data = json.load(fil)
+
+        self.config = data
+        self.start_urls = data['start_urls']
+        self.allowed_domains = data['allowed_domains']
 
     def parse(self, response):
         return [FormRequest.from_response(
-            response, formdata={'email': 'bernard', 'password': 'abcd1234'},
+            response, formdata={
+                'email': self.config['email'],
+                'password': self.config['password']},
             callback=self.after_login)]
 
     def after_login(self, response):
         # check login succeed before going on
         sel = Selector(response)
-        # ipdb.set_trace()
-        if sel.css('.alert-error'):
+        if sel.css(self.config['login_error_selector']):
             log.msg('Login failed', level=log.ERROR)
             return
 
         # continue scraping with authenticated session
         log.msg('Login success', level=log.INFO)
+        url_prefix = urlparse.urlparse(response.url)
 
         for url in sel.css('a::attr(href)').extract():
-            if url != '/logout/':
-                yield Request(url='http://peopleask.local' + url,
-                              callback=self.parse_page)
+            #parsed_url = urlparse.urlparse(url)
 
-    def parse_page(self, response):
-        sel = Selector(response)
-        for url in sel.css('a::attr(href)').extract():
-            if url != '/logout/':
-                yield Request(url='http://peopleask.local' + url,
-                              callback=self.parse_page)
-
-
-class PeopleAskManager(Spider):
-    name = 'peopleask_manager'
-    allowed_domains = ['peopleask.local']
-    start_urls = ['http://peopleask.local/manager']
-
-    def parse(self, response):
-        return [FormRequest.from_response(
-            response, formdata={'email': 'admin', 'password': 'admin1234'},
-            callback=self.after_login)]
-
-    def after_login(self, response):
-        # check login succeed before going on
-        sel = Selector(response)
-        # ipdb.set_trace()
-        if sel.css('.errorlist'):
-            log.msg('Login failed', level=log.ERROR)
-            return
-
-        # continue scraping with authenticated session
-        log.msg('Login success', level=log.INFO)
-
-        for url in sel.css('a::attr(href)').extract():
-            yield Request(url='http://peopleask.local' + url,
-                          callback=self.parse_page)
+            try:
+                yield Request(url, callback=self.parse_page)
+            except ValueError:
+                url_final = '{scheme}://{netloc}{path}'.format(
+                    scheme=url_prefix.scheme, netloc=url_prefix.netloc,
+                    path=url)
+                yield Request(url_final, callback=self.parse_page)
 
     def parse_page(self, response):
         log.msg('Status code page {status_code} for {url}'.format(
             status_code=response.status, url=response.url), level=log.INFO)
         sel = Selector(response)
+        url_prefix = urlparse.urlparse(response.url)
+
         for url in sel.css('a::attr(href)').extract():
-            yield Request(url='http://peopleask.local' + url,
-                          callback=self.parse_page)
+            try:
+                yield Request(url, callback=self.parse_page)
+            except ValueError:
+                url_final = '{scheme}://{netloc}{path}'.format(
+                    scheme=url_prefix.scheme, netloc=url_prefix.netloc,
+                    path=url)
+                yield Request(url_final, callback=self.parse_page)
