5f08db57f8daabd515cfb97d6217135913a7f39b
hobbestigrou <natal.ngetal@novapost.fr>
2014-11-14 15:41:25 +0100

    [Tests] Typo.

--- tests/basic.py
+++ tests/basic.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-'''Simple basic test for JSON'''
+"""Simple basic test for JSON"""
 
 import unittest
 
@@ -8,13 +8,14 @@ from crappyspider.spiders.crappyspider import CrappySpider
 
 
 class CrappySpiderTestCase(unittest.TestCase):
-    '''Basic test class with simple test'''
+    """Basic test class with simple test"""
 
     def test_without_arg(self):
-        '''Simple test no placeholders'''
+        """Test initialize withtout argument."""
         self.failUnlessRaises(ValueError, CrappySpider)
 
     def test_with_insufficient_arg(self):
+        """Test with insufficient argument."""
         config = None
         output_format = 'json',
         output_filename = 'output'
@@ -23,6 +24,7 @@ class CrappySpiderTestCase(unittest.TestCase):
                               output_filename, start_urls)
 
     def test_allowed_domain(self):
+        """Test with insufficient argument."""
         config = None
         output_format = 'json',
         output_filename = 'output'
