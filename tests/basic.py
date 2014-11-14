b8fc9835100b380190441e25c4d4fda065622ba3
hobbestigrou <natal.ngetal@novapost.fr>
2014-11-14 15:39:06 +0100

    [Tests] Add few tests (fixes #27).

---
+++ tests/basic.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''Simple basic test for JSON'''
+
+import unittest
+
+from crappyspider.spiders.crappyspider import CrappySpider
+
+
+class CrappySpiderTestCase(unittest.TestCase):
+    '''Basic test class with simple test'''
+
+    def test_without_arg(self):
+        '''Simple test no placeholders'''
+        self.failUnlessRaises(ValueError, CrappySpider)
+
+    def test_with_insufficient_arg(self):
+        config = None
+        output_format = 'json',
+        output_filename = 'output'
+        start_urls = 'http://test.com'
+        self.failUnlessRaises(ValueError, CrappySpider, config, output_format,
+                              output_filename, start_urls)
+
+    def test_allowed_domain(self):
+        config = None
+        output_format = 'json',
+        output_filename = 'output'
+        start_urls = None
+        allowed_domains = 'test.com'
+        self.failUnlessRaises(ValueError, CrappySpider, config, output_format,
+                              output_filename, start_urls, allowed_domains)
