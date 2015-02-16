426349519c8f8312422b6f4ab5003d1702bd937a
hobbestigrou <natal.ngetal@novapost.fr>
2015-02-16 12:11:20 +0100

    Back to development: 0.4

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ except ImportError:
 setup(
     name="crappyspider",
     packages=["crappyspider"],
-    version='0.3',
+    version='0.4.dev0',
     install_requires=['scrapy'],
     description="Test your site.",
     author="Peopledoc",
